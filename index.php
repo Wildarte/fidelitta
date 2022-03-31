bf2f5d5c0e5b3eb656b6840de32ae4c10c653c76
wildarte <wildarte10@gmail.com>
2022-03-31 19:57:45 -0300

    avanço da pagina

--- index.php
+++ index.php
@@ -4,11 +4,298 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <link rel="stylesheet" href="assets/bootstrap-icons-1.7.2/bootstrap-icons.css">
+    <link rel="stylesheet" href="assets/css/owl.carousel.min.css">
+    <link rel="stylesheet" href="assets/css/owl.theme.default.min.css">
+    <link rel="stylesheet" href="assets/css/reset.css">
+    <link rel="stylesheet" href="assets/css/style.css">
+    <title>Fidelitta</title>
 </head>
 <body>
     
-    
+    <header class="header container_full">
 
+        <div class="header_content container">
+            <div class="header_top">
+                <div class="header_logo">
+                    <a href="">
+                        <img src="assets/img/logo.png" alt="">
+                    </a>
+                </div>
+
+                <div class="header_unities">
+                    <a href="#unidades"><strong>Unidade 1:</strong> Porto Alegre <i class="bi bi-caret-down-fill"></i></a>
+                    <a href="#unidades"><strong>Unidade 2:</strong> Caxias do Sul <i class="bi bi-caret-down-fill"></i></a>
+                </div>
+
+                <div class="header_social">
+                    <a href="#"><i class="bi bi-facebook"></i></a>
+                    <a href="#"><i class="bi bi-instagram"></i></a>
+                    <a href="#"><i class="bi bi-youtube"></i></a>
+                </div>
+            </div>
+
+        </div>
+
+        <div class="header_bottom">
+            <div class="header_bottom_content container">
+                <nav class="menu">
+                    <ul>
+                        <li><a href="#">Home</a></li>
+                        <li><a href="#">Sobre</a></li>
+                        <li><a href="#">Atendimento</a></li>
+                        <li><a href="#">O que está esperando?</a></li>
+                        <li><a href="#">Unidades</a></li>
+                        <li><a href="#">Contato</a></li>
+                        <li><a href="#">Fidelitta TV</a></li>
+                    </ul>
+                </nav>
+
+                <div class="header_cta">
+                    <a href="#">Contato</a>
+                </div>
+            </div>
+                
+        </div>
+
+    </header>
+
+
+    <main>
+
+        <section class="sec_intro">
+            <div class="owl-carousel owl-theme" style="width: 100%; height: 580px; overflow: hidden">
+                <img style="object-fit: cover; height: 580px" src="assets/img/slider1.jpg" alt="">
+                <img style="object-fit: cover; height: 580px" src="assets/img/slider2.jpg" alt="">
+                <img style="object-fit: cover; height: 580px" src="assets/img/slider3.jpg" alt="">
+            </div>
+        </section>
+
+        <section class="sec_sobre container_full">
+            <div class="sobre_left" style="background: url('assets/img/about.jpg') top right">
+                
+            </div>
+            <div class="sobre_right">
+                <header>
+                    <h2>Sobre a Fidelittá</h2>
+                </header>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis perspiciatis aspernatur rem ducimus illo a consectetur sapiente veniam, tenetur ipsa fugit ut tempore quasi maxime excepturi quas laudantium optio voluptate quaerat ex sint minus! Quos, provident culpa quod laboriosam vitae praesentium enim accusamus. Dolore rerum ad animi perspiciatis, laboriosam temporibus facere porro iste! Quo cumque tempora, consequuntur sit eligendi esse.</p>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere, ut dignissimos magnam debitis expedita necessitatibus illum adipisci tempora asperiores velit minus voluptate alias itaque atque? Obcaecati reiciendis quibusdam quasi ab!</p>
+                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sit rem odio molestias molestiae iusto aspernatur tempora mollitia quibusdam soluta nobis, voluptas porro repellendus debitis unde error corporis pariatur numquam doloremque dolore, vero perspiciatis tenetur a non? Ratione labore dolor odio.</p>
+            </div>
+            
+        </section>
+
+        <section>
+
+            <header>
+                <h3>Atendimento</h3>
+            </header>
+
+            <section>
+                <article>
+                    <header>
+                        <h4>Entrevista</h4>
+                    </header>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam tempore fugit veritatis laboriosam perspiciatis, itaque nihil ut suscipit vero quos nemo debitis laudantium vitae nulla, non reiciendis. Recusandae, ab quo.</p>
+                </article>
+
+                <article>
+                    <header>
+                        <h4>Cadastro</h4>
+                    </header>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam magni suscipit dolorem earum? At dignissimos, ullam nulla ipsa quas deleniti accusantium ad rem non sequi eum aperiam? Est, nesciunt maiores?</p>
+                </article>
+
+                <article>
+                    <header>
+                        <h4>Segurança</h4>
+                    </header>
+                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptas corrupti praesentium, ducimus excepturi fuga odio sequi. Dignissimos enim aperiam, unde nostrum, esse id nemo reprehenderit tenetur autem laboriosam itaque ex!</p>
+                </article>
+            </section>
+            
+        </section>
+
+
+        <section>
+            
+            <div>
+                <header>
+                    <h3>O que você está esperando</h3>
+                </header>
+
+                <div>
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam, distinctio. Velit exercitationem accusamus similique. Esse adipisci natus veritatis consequatur necessitatibus, suscipit magni error, cumque eos, aspernatur ut dolor? Deserunt necessitatibus libero dolore excepturi a quasi culpa eveniet, corporis blanditiis cupiditate.
+                    </p>
+                </div>
+            </div>
+
+        </section>
+
+
+
+        <section>
+
+            <div>
+                
+                <div class="card_unity">
+                    <div class="card_unity_left">
+                        <div>
+                            <div>
+                                <header>
+                                    <h4>Unidade 1</h4>
+                                </header>
+
+                                <ul>
+                                    <li>Rua Tobias da Silva, 22 conj. 402 | Moinhos de Vento</li>
+                                    <li>De seg. às sextas feiras, das 8:30h às 12:00h e das 13:30h às 19:00h;</li>
+                                    <li>Um sábado por mês, atendemos entrevistas mediante agendamento prévio;</li>
+                                    <li>Aceitamos todos os cartões de crédito;</li>
+                                </ul>
+
+                                <div>
+                                    <a href="#">(99) 9 9999-9999</a>
+
+                                    <a href="#">(99) 9999-9999</a>
+                                </div>
+                            </div>
+                            <div class="card_right">
+
+                            </div>
+                        </div>
+
+                        <div>
+                            mapa
+                        </div>
+                        
+                    </div>
+                </div>
+
+
+                <div class="card_unity">
+                    <div class="card_unity_left">
+                        <div>
+                            <div>
+                                <header>
+                                    <h4>Unidade 1</h4>
+                                </header>
+
+                                <ul>
+                                    <li>Rua Tobias da Silva, 22 conj. 402 | Moinhos de Vento</li>
+                                    <li>De seg. às sextas feiras, das 8:30h às 12:00h e das 13:30h às 19:00h;</li>
+                                    <li>Um sábado por mês, atendemos entrevistas mediante agendamento prévio;</li>
+                                    <li>Aceitamos todos os cartões de crédito;</li>
+                                </ul>
+
+                                <div>
+                                    <a href="#">(99) 9 9999-9999</a>
+
+                                    <a href="#">(99) 9999-9999</a>
+                                </div>
+                            </div>
+                            <div class="card_right">
+
+                            </div>
+                        </div>
+
+                        <div>
+                            mapa
+                        </div>
+                        
+                    </div>
+                </div>
+
+            </div>
+
+        </section>
+
+
+        <section class="sec_contato">
+
+            <div class="contato_content">
+
+                <div class="form_contato">
+
+                    <div class="img_form">
+
+                    </div>
+
+                    <form action="">
+
+                    </form>
+
+                </div>
+
+            </div>
+
+        </section>
+
+    </main>
+
+
+    <footer class="footer">
+
+        <div class="footer_content">
+
+            <div>
+                <img src="" alt="">
+            </div>
+
+            <div>
+                <header>
+                    <h4>Unidades</h4>
+                </header>
+
+                <ul>
+                    <li>
+                        <h5> <strong>Unidade 1:</strong> Porto Alegre</h5>
+                        <p>Rua Tobias da Silva, 22 - Conj 402 | Moinhos de Vento | Porto Alegre-RS </p>
+                        <p> <a href="tel:+5599999999999">(99) 9 9999-9999</a> </p>
+                        <p> <a href="tel:+5599999999999">(99) 9 9999-9999</a> </p>
+                        <p> <a href="mailto:contato@email.com">contato@email.com</a> </p>
+                    </li>
+                </ul>
+            </div>
+
+            <div>
+
+                <header>
+                    <h4>redes Sociais</h4>
+                </header>
+
+                <ul>
+                    <li>
+                        <a href="http://"></a>
+                    </li>
+                </ul>
+            </div>
+
+        </div>
+        <div>
+            <p>Nós <span>&#10084;</span> Formar Casais!</p>
+            <p>FIDELITTÁ ASSESSORIA AFETIVA ® - AGÊNCIA DE RELACIONAMENTOS DE PORTO ALEGRE - RS | <?= date("Y") ?></p>
+        </div>
+    </footer>
+
+    <script src="assets/js/jquery-3.6.0.min.js"></script>
+    <script src="assets/js/owl.carousel.min.js"></script>
+    <script src="assets/js/script.js"></script>
+    <script>
+        $('.owl-carousel').owlCarousel({
+            loop:true,
+            margin:10,
+            nav:true,
+            autoplay: true,
+            autoplayTimeout:3000,
+            autoplayHoverPause:true,
+            responsive:{
+                0:{
+                    items:1
+                }
+            }
+        })
+    </script>
 </body>
 </html>
